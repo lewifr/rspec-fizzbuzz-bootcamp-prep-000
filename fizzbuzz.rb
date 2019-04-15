c630d8736162e3056b549ac6a8a0e82fa4e058ed
Francis Lewis <francis.lewis@me.com>
2019-04-15 13:47:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 15 == 0
+    "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 15 == 0
+  elsif int % 3 == 0
     "FizzBuzz"
   else
     nil
